00ede1355b56d45b5d8fb3433e71fb90c1d5b6dc
Shalom123-debug-art <shalom12github@gmail.com>
2025-11-07 19:02:53 +0300

    Update Exercise 3: Divisibility Check Program.py

--- Exercise 3: Divisibility Check Program.py
+++ Exercise 3: Divisibility Check Program.py
@@ -1,1 +1,10 @@
 
+# Excerice 3: Divisbility Check Program
+num = int(input("Enter a number"))
+def divisibility_by_5_Checker(num):
+  if (num % 5 == 0):
+    print("\nDivisible by 5")
+  else:
+    print("\nNot divisible by 5")
+divisibility_by_5_Checker(num)
+  
