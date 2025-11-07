c693c60f1224484e4ee5cf989221a3c85757e695
Shalom123-debug-art <shalom12github@gmail.com>
2025-11-07 19:41:24 +0300

    Update Exercise 6: ATM Simulator.py

--- Exercise 6: ATM Simulator.py
+++ Exercise 6: ATM Simulator.py
@@ -1,1 +1,19 @@
 
+# Exercise 6: ATM Simulator 
+withdrawal_amount = float(input("Enter the amount you want to withdraw"))
+def atm_withdrawal(amount):
+  balance = 1000
+  if amount > balance:
+    print("\nYou can't withdraw more than what you have!")
+    return
+  elif amount < 0:
+    print("\nYou can't withdraw negative amount of money!")
+    return
+  elif amount == 0:
+    print("\nYou can't withdraw nothing!")
+    return
+  else:
+    balance -= amount
+    print(f"\nYou've withdrawn {amount} birr, and your current balance is {round(balance, 2)} birr.")
+
+atm_withdrawal(withdrawal_amount)
