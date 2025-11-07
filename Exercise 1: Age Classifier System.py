31294a15e6dbd887fb8afe4fa80e37aae6cdfdee
Shalom123-debug-art <shalom12github@gmail.com>
2025-11-07 19:02:18 +0300

    Update Exercise 1: Age Classifier System.py

--- Exercise 1: Age Classifier System.py
+++ Exercise 1: Age Classifier System.py
@@ -1,1 +1,17 @@
 
+# Exercise 1: Age Classifier System
+age = int(input("Enter your age"))
+def classify_person(age):
+  match age:
+    case _ if 65 <= age:
+      print("\nSenior")
+    case _ if 18 <= age <= 64:
+      print("\nAdult")
+    case _ if 13 <= age <= 17:
+      print("\nTeenager")
+    case _ if 0 <= age <= 12:
+      print("\nChild")
+    case _:
+      print("\nYou're not born yet")
+classify_person(age)
+
