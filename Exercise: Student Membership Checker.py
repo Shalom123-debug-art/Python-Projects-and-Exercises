88a7efef921bdcea4ec1cc49d41c33940bf6501d
Shalom123-debug-art <shalom12github@gmail.com>
2025-11-20 17:46:41 +0300

    Update Exercise: Student Membership Checker.py

--- Exercise: Student Membership Checker.py
+++ Exercise: Student Membership Checker.py
@@ -1,1 +1,15 @@
 
+# Exercise: Student Membership Checker
+students_list = ["Saul", "David", "James", "Moses"]
+student_input = input("Enter the name of the student that you want to look up")
+
+def student_name_checker(student):
+  for i in range(len(students_list)):
+    if student == students_list[i]:
+      print("\nThe student you mentioned is found!")
+      print(f"Student {i + 1} is {students_list[i]}")
+      return
+  print(f"\nThere's no student called {student} in the list.")
+  
+student_name_checker(student_input)
+
